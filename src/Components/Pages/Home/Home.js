dcda4eb1688f264d8511f1fbe3f84df0fee81f68
rakib ahmed <rakibahmed.dev@gmail.com>
2022-05-26 22:49:45 +0600

    marketing campaigns donw in home

--- src/Components/Pages/Home/Home.js
+++ src/Components/Pages/Home/Home.js
@@ -1,12 +1,14 @@
 import React from "react";
 import PartsForHome from "../Products/PartsForHome";
 import Landing from "./Landing";
+import Marketing from "./Marketing";
 
 const Home = () => {
   return (
     <div>
       <Landing></Landing>
       <PartsForHome></PartsForHome>
+      <Marketing></Marketing>
     </div>
   );
 };
--- src/Components/Pages/Products/PartsForHome.js
+++ src/Components/Pages/Products/PartsForHome.js
@@ -8,7 +8,7 @@ function PartsForHome() {
 
   return (
     <div className="px-20 py-14 bg-slate-50">
-      <h1 className=" text-2xl text-center font-medium sm:text-6xl mb-20">
+      <h1 className=" text-2xl text-center font-medium sm:text-6xl mb-14">
         Products
       </h1>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-16">
